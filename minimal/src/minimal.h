5b0c43de89b5aaaba10d7805b1bb152aac251c5a
andrewjwilkinson <andrewjwilkinson@64379b1e-0c1e-0410-9f5b-b10fb511c92e>
2006-10-12 10:22:45 +0000

    Initial checkin for Minimal language

---
+++ minimal/src/minimal.h
@@ -0,0 +1,33 @@
+/*
+*    Copyright 2006 Andrew Wilkinson <aw@cs.york.ac.uk>.
+*
+*    This file is part of Minimal (http://www-users.cs.york.ac.uk/~aw/pylinda)
+*
+*    Minimal is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation; either version 2.1 of the License, or
+*    (at your option) any later version.
+*
+*    Minimal is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License
+*    along with Minimal; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef MINIMAL_H
+
+MinimalLayer Minimal_createLayer();
+
+MinimalFunction Minimal_parseTypeSpec(char* code);
+MinimalFunction Minimal_parseCode(char* code);
+
+MinimalFunction Minimal_getFunction(char* funcname);
+
+void Minimal_addReference(MinimalObject* obj);
+void Minimal_delReference(MinimalObject* obj);
+
+#endif
